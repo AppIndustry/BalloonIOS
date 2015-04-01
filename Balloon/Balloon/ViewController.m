95e0d99d029b2eaffc7fd5a8334ef6474a63b22f
MunFai <munfai@live.com>
2015-04-01 22:59:15 +0800

    Add base project
    add a base project for the app

---
+++ Balloon/Balloon/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Balloon
+//
+//  Created by Mun Fai Leong on 4/1/15.
+//  Copyright (c) 2015 myAppIndustry. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ Balloon/Balloon/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Balloon
+//
+//  Created by Mun Fai Leong on 4/1/15.
+//  Copyright (c) 2015 myAppIndustry. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
